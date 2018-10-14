4fd48c1578229c318c431c758173b1c9a1efec09
Avinash <avinash.kappa@gmail.com>
2018-10-14 10:13:06 +0530

    before restart

--- web/web-client/js/senddata.js
+++ web/web-client/js/senddata.js
@@ -2,28 +2,6 @@ $(document).ready(function() {
 
 	console.log( "ready!" );
 
-	$("#lalalala").click(function(event){
-		alert("senddatabutton clicked");
-		$.ajax({
-			url: '/newuserdata',
-            enctype: 'multipart/form-data',
-            data: {a: 'a', b: 'b'},
-            type: "POST", //ADDED THIS LINE
-            // ... Other options like success and etc
-            success: function(msg){
-                //alert(msg);
-                document.getElementById("my_camera_div").style.display = "block";
-                document.getElementById("my_result_div").style.display = "none";
-                alert(msg);
-            },
-            error: function(msg){
-                alert("failure : "+msg);
-                document.getElementById("my_camera_div").style.display = "block";
-                document.getElementById("my_result_div").style.display = "none";
-            }
-		});
-	});
-
 
 	$('#opt1').click(function(){
 		document.getElementById("oldusersenddatabutton").style.display = "block";
